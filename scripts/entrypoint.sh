1ded107825a9eb770938fd18117828f0faeba4f9
Andrew Erickson <aerickson@mozilla.com>
2019-02-20 14:20:03 -0800

    more variables

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -15,18 +15,20 @@ if [[ -e /test ]]; then
     ls -la /test
 fi
 
-WRITABLE_PATH='/builds/taskcluster/'
+WORK_PATH='/builds/taskcluster/'
+export ED25519_PRIVKEY="$WORK_PATH/ed25519_private_key"
+export OPENPGP_PRIVKEY="$WORK_PATH/openpgp_private_key"
 # we're not using livelog yet, set key to something so g-w will start
 export LIVELOG_SECRET='not_a_key'
 
 entrypoint.py
 cd $HOME
-generic-worker new-ed25519-keypair --file $WRITABLE_PATH/ed25519_private_key
-generic-worker new-openpgp-keypair --file $WRITABLE_PATH/openpgp_private_key
-envsubst < /builds/generic-worker/generic-worker.yml.template > $WRITABLE_PATH/generic-worker.yml
+generic-worker new-ed25519-keypair --file $ED25519_PRIVKEY
+generic-worker new-openpgp-keypair --file $OPENPGP_PRIVKEY
+envsubst < $WORK_PATH/generic-worker.yml.template > $WORK_PATH/generic-worker.yml
 
 # user.Current requires cgo, but cross-compilation doesn't enable cgo
 # this works around the issue (see https://github.com/golang/go/issues/14625
 # and https://github.com/ksonnet/ksonnet/issues/298)
 export USER=root
-exec generic-worker run --config $WRITABLE_PATH/generic-worker.yml
+exec generic-worker run --config $WORK_PATH/generic-worker.yml
