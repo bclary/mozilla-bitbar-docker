c655b2cbaf7de71f31842634ab607d5fee4b5348
Andrew Erickson <aerickson@mozilla.com>
2019-03-25 15:24:25 -0700

    remove usage comment in favor of official -h

--- taskcluster/script.py
+++ taskcluster/script.py
@@ -4,8 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# usage: ./script.py ./test_command --option1 blue --option2 green
-
 import argparse
 import json
 import logging
