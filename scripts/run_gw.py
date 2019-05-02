2eada5df9b5b56be2211990362c113cfb73e4d79
Andrew Erickson <aerickson@mozilla.com>
2019-05-02 12:27:16 -0700

    add pt logging

--- scripts/run_gw.py
+++ scripts/run_gw.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 
-import os
 import json
+import logging
+import os
+import socket
 import subprocess
 import sys
+
+import google.cloud.logging
 
 # runs a command and:
 # - print to stdout & stderr
 # - log to papertrail
+
+cmd_str = "generic-worker run --config %s" % gw_config_file
+cmd_arr = cmd_str.split(" ")
+# testing mode
+# cmd_arr = sys.argv[1:]
+
+hostname = socket.gethostname()
+
+def log_to_pt(message):
+    # TODO: prefix log messages with host?
+    logging.info("%s: %s" % (hostname, message))
+
+# setup stackdriver
+stackdriver_client = google.cloud.logging.Client()
+stackdriver_client.setup_logging()
 
 script_name = sys.argv[0]
 scriptvars_json_file = '/builds/taskcluster/scriptvars.json'
@@ -19,13 +38,9 @@ if os.path.exists(scriptvars_json_file):
     with open(scriptvars_json_file) as json_file:
         scriptvars_json = json.load(json_file)
 
-cmd_str = "generic-worker run --config %s" % gw_config_file
-cmd_arr = cmd_str.split(" ")
-# testing mode
-# cmd_arr = sys.argv[1:]
-
 print("%s: command to run is: '%s'" % (script_name, " ".join(cmd_arr)))
 
+# run command
 rc = None
 proc = subprocess.Popen(cmd_arr,
                         env=scriptvars_json,
@@ -35,6 +50,6 @@ proc = subprocess.Popen(cmd_arr,
 while rc == None:
     line = proc.stdout.readline()
     sys.stdout.write(line)
-    # TODO: log to papertrail
+    log_to_pt(line)
     rc = proc.poll()
 sys.exit(rc)
