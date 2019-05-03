6d63f6e0df0ddab3e80dadfc7a84251aa491e78b
Andrew Erickson <aerickson@mozilla.com>
2019-05-03 15:11:52 -0700

    undo changes to script.py

--- taskcluster/script.py
+++ taskcluster/script.py
@@ -14,160 +14,147 @@ import sys
 from distutils.dir_util import copy_tree
 from glob import glob
 
-import google.cloud.logging
 from mozdevice import ADBAndroid, ADBHost, ADBError
 
 
 MAX_NETWORK_ATTEMPTS = 3
 
-class ScriptPy:
 
-    def __init__(self):
-        self.stackdriver_client = google.cloud.logging.Client()
-        self.stackdriver_client.setup_logging()
+def fatal(message):
+    """Emit an error message and exit the process with status
+    TBPL_RETRY_EXIT_STATUS this will cause the job to be retried.
 
-    def fatal(self, message):
-        """Emit an error message and exit the process with status
-        TBPL_RETRY_EXIT_STATUS this will cause the job to be retried.
+    """
+    # Exit Code 1 until we move to generic-worker since
+    # taskcluster-worker reserves 4 for internal worker errors.
+    #TBPL_RETRY_EXIT_STATUS = 4
+    TBPL_RETRY_EXIT_STATUS = 1
+    print('TEST-UNEXPECTED-FAIL | bitbar | {}'.format(message))
+    sys.exit(TBPL_RETRY_EXIT_STATUS)
 
-        """
-        # Exit Code 1 until we move to generic-worker since
-        # taskcluster-worker reserves 4 for internal worker errors.
-        #TBPL_RETRY_EXIT_STATUS = 4
-        TBPL_RETRY_EXIT_STATUS = 1
-        self.print_and_log('TEST-UNEXPECTED-FAIL | bitbar | {}'.format(message))
-        sys.exit(TBPL_RETRY_EXIT_STATUS)
+def main():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options] <test command> (<test command option> ...)',
+        description="Wrapper script for tests run on physical Android devices at Bitbar. Runs the provided command wrapped with required setup and teardown.")
+    _args, extra_args = parser.parse_known_args()
+    logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s',
+                        level=logging.INFO,
+                        stream=sys.stdout)
 
-    # prints and logs to stackdriver
-    def print_and_log(self, message):
-        # TODO: prefix log messages with host?
-        logging.info(message)
-        print(message)
+    print('\nBegin script.py')
+    with open('/builds/worker/version') as versionfile:
+        version = versionfile.read().strip()
+    print('\nDockerfile version {}'.format(version))
 
-    def main(self):
-        parser = argparse.ArgumentParser(
-            usage='%(prog)s [options] <test command> (<test command option> ...)',
-            description="Wrapper script for tests run on physical Android devices at Bitbar. Runs the provided command wrapped with required setup and teardown.")
-        _args, extra_args = parser.parse_known_args()
-        logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s',
-                            level=logging.INFO,
-                            stream=sys.stdout)
+    taskcluster_debug = '*'
 
-        self.print_and_log('\nBegin script.py')
-        with open('/builds/worker/version') as versionfile:
-            version = versionfile.read().strip()
-        self.print_and_log('\nDockerfile version {}'.format(version))
+    task_cwd = os.getcwd()
+    print('Current working directory: {}'.format(task_cwd))
 
-        taskcluster_debug = '*'
+    with open('/builds/taskcluster/scriptvars.json') as scriptvars:
+        scriptvarsenv = json.loads(scriptvars.read())
+        print('Bitbar test run: https://mozilla.testdroid.com/#testing/device-session/{}/{}/{}'.format(
+            scriptvarsenv['TESTDROID_PROJECT_ID'],
+            scriptvarsenv['TESTDROID_BUILD_ID'],
+            scriptvarsenv['TESTDROID_RUN_ID']))
 
-        task_cwd = os.getcwd()
-        self.print_and_log('Current working directory: {}'.format(task_cwd))
+    env = dict(os.environ)
 
-        with open('/builds/taskcluster/scriptvars.json') as scriptvars:
-            scriptvarsenv = json.loads(scriptvars.read())
-            self.print_and_log('Bitbar test run: https://mozilla.testdroid.com/#testing/device-session/{}/{}/{}'.format(
-                scriptvarsenv['TESTDROID_PROJECT_ID'],
-                scriptvarsenv['TESTDROID_BUILD_ID'],
-                scriptvarsenv['TESTDROID_RUN_ID']))
+    if 'PATH' in os.environ:
+        path = os.environ['PATH']
+    else:
+        path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 
-        env = dict(os.environ)
+    path += ':/builds/worker/android-sdk-linux/tools/bin:/builds/worker/android-sdk-linux/platform-tools'
 
-        if 'PATH' in os.environ:
-            path = os.environ['PATH']
-        else:
-            path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+    env['PATH'] = os.environ['PATH'] = path
+    env['NEED_XVFB'] = 'false'
+    env['DEVICE_NAME'] = scriptvarsenv['DEVICE_NAME']
+    env['ANDROID_DEVICE'] = scriptvarsenv['ANDROID_DEVICE']
+    env['DEVICE_SERIAL'] = scriptvarsenv['DEVICE_SERIAL']
+    env['HOST_IP'] = scriptvarsenv['HOST_IP']
+    env['DEVICE_IP'] = scriptvarsenv['DEVICE_IP']
 
-        path += ':/builds/worker/android-sdk-linux/tools/bin:/builds/worker/android-sdk-linux/platform-tools'
+    if 'HOME' not in env:
+        env['HOME'] = '/builds/worker'
+        print('setting HOME to {}'.format(env['HOME']))
 
-        env['PATH'] = os.environ['PATH'] = path
-        env['NEED_XVFB'] = 'false'
-        env['DEVICE_NAME'] = scriptvarsenv['DEVICE_NAME']
-        env['ANDROID_DEVICE'] = scriptvarsenv['ANDROID_DEVICE']
-        env['DEVICE_SERIAL'] = scriptvarsenv['DEVICE_SERIAL']
-        env['HOST_IP'] = scriptvarsenv['HOST_IP']
-        env['DEVICE_IP'] = scriptvarsenv['DEVICE_IP']
+    # If we are running normal tests we will be connected via usb and
+    # there should be only one device connected.  If we are running
+    # power tests, the framework will have already called adb tcpip
+    # 5555 on the device before it disconnected usb. There should be
+    # no devices connected and we will need to perform an adb connect
+    # to connect to the device. DEVICE_SERIAL will be set to either
+    # the device's serial number or its ipaddress:5555 by the framework.
+    try:
+        adbhost = ADBHost()
+        if env['DEVICE_SERIAL'].endswith(':5555'):
+            # Power testing with adb over wifi.
+            adbhost.command_output(["connect", env['DEVICE_SERIAL']])
+        devices = adbhost.devices()
+        print(json.dumps(devices, indent=4))
+        if len(devices) != 1:
+            fatal('Must have exactly one connected device. {} found.'.format(len(devices)))
+    except ADBError as e:
+        fatal('{} Unable to obtain attached devices'.format(e))
 
-        if 'HOME' not in env:
-            env['HOME'] = '/builds/worker'
-            self.print_and_log('setting HOME to {}'.format(env['HOME']))
+    try:
+        for f in glob('/tmp/adb.*.log'):
+            print('\n{}:\n'.format(f))
+            with open(f) as afile:
+                print(afile.read())
+    except Exception as e:
+        print('{} while reading adb logs'.format(e))
 
-        # If we are running normal tests we will be connected via usb and
-        # there should be only one device connected.  If we are running
-        # power tests, the framework will have already called adb tcpip
-        # 5555 on the device before it disconnected usb. There should be
-        # no devices connected and we will need to perform an adb connect
-        # to connect to the device. DEVICE_SERIAL will be set to either
-        # the device's serial number or its ipaddress:5555 by the framework.
-        try:
-            adbhost = ADBHost()
-            if env['DEVICE_SERIAL'].endswith(':5555'):
-                # Power testing with adb over wifi.
-                adbhost.command_output(["connect", env['DEVICE_SERIAL']])
-            devices = adbhost.devices()
-            self.print_and_log(json.dumps(devices, indent=4))
-            if len(devices) != 1:
-                self.fatal('Must have exactly one connected device. {} found.'.format(len(devices)))
-        except ADBError as e:
-            self.fatal('{} Unable to obtain attached devices'.format(e))
+    print('Connecting to Android device {}'.format(env['DEVICE_SERIAL']))
+    try:
+        device = ADBAndroid(device=env['DEVICE_SERIAL'])
 
-        try:
-            for f in glob('/tmp/adb.*.log'):
-                self.print_and_log('\n{}:\n'.format(f))
-                with open(f) as afile:
-                    self.print_and_log(afile.read())
-        except Exception as e:
-            self.print_and_log('{} while reading adb logs'.format(e))
+        # clean up the device.
+        device.rm('/data/local/tests', recursive=True, force=True, root=True)
+        device.rm('/data/local/tmp/*', recursive=True, force=True, root=True)
+        device.rm('/data/local/tmp/xpcb', recursive=True, force=True, root=True)
+        device.rm('/sdcard/tests', recursive=True, force=True, root=True)
+        device.rm('/sdcard/raptor-profile', recursive=True, force=True, root=True)
+    except ADBError as e:
+        fatal("{} attempting to clean up device".format(e))
 
-        self.print_and_log('Connecting to Android device {}'.format(env['DEVICE_SERIAL']))
-        try:
-            device = ADBAndroid(device=env['DEVICE_SERIAL'])
+    if taskcluster_debug:
+        env['DEBUG'] = taskcluster_debug
 
-            # clean up the device.
-            device.rm('/data/local/tests', recursive=True, force=True, root=True)
-            device.rm('/data/local/tmp/*', recursive=True, force=True, root=True)
-            device.rm('/data/local/tmp/xpcb', recursive=True, force=True, root=True)
-            device.rm('/sdcard/tests', recursive=True, force=True, root=True)
-            device.rm('/sdcard/raptor-profile', recursive=True, force=True, root=True)
-        except ADBError as e:
-            self.fatal("{} attempting to clean up device".format(e))
+    print('environment = {}'.format(json.dumps(env, indent=4)))
 
-        if taskcluster_debug:
-            env['DEBUG'] = taskcluster_debug
+    # run the payload's command
+    print(' '.join(extra_args))
+    rc = None
+    proc = subprocess.Popen(extra_args,
+                            env=env,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    while rc == None:
+        line = proc.stdout.readline()
+        sys.stdout.write(line)
+        rc = proc.poll()
 
-        self.print_and_log('environment = {}'.format(json.dumps(env, indent=4)))
+    try:
+        if env['DEVICE_SERIAL'].endswith(':5555'):
+            device.command_output(["usb"])
+            adbhost.command_output(["disconnect", env['DEVICE_SERIAL']])
+        adbhost.kill_server()
+    except ADBError as e:
+        print('{} attempting adb kill-server'.format(e))
 
-        # run the payload's command
-        self.print_and_log(' '.join(extra_args))
-        rc = None
-        proc = subprocess.Popen(extra_args,
-                                env=env,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-        while rc == None:
-            line = proc.stdout.readline()
-            sys.stdout.write(line)
-            rc = proc.poll()
+    try:
+        print('\nnetstat -aop\n%s\n\n' % subprocess.check_output(
+            ['netstat', '-aop'],
+            stderr=subprocess.STDOUT))
+    except subprocess.CalledProcessError as e:
+        print('{} attempting netstat'.format(e))
 
-        try:
-            if env['DEVICE_SERIAL'].endswith(':5555'):
-                device.command_output(["usb"])
-                adbhost.command_output(["disconnect", env['DEVICE_SERIAL']])
-            adbhost.kill_server()
-        except ADBError as e:
-            self.print_and_log('{} attempting adb kill-server'.format(e))
-
-        try:
-            self.print_and_log('\nnetstat -aop\n%s\n\n' % subprocess.check_output(
-                ['netstat', '-aop'],
-                stderr=subprocess.STDOUT))
-        except subprocess.CalledProcessError as e:
-            self.print_and_log('{} attempting netstat'.format(e))
-
-        self.print_and_log('script.py exitcode {}'.format(rc))
-        if rc == 0:
-            return 0
-        return 1
+    print('script.py exitcode {}'.format(rc))
+    if rc == 0:
+        return 0
+    return 1
 
 if __name__ == "__main__":
-    sp = ScriptPy()
-    sys.exit(sp.main())
+    sys.exit(main())
