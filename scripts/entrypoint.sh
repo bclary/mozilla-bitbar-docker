2970dc7da632466b8d6e3465ce52b2d35ce81af2
Andrew Erickson <aerickson@mozilla.com>
2019-05-03 15:09:11 -0700

    we don't need to write out the env var any longer

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -33,9 +33,6 @@ generic-worker new-ed25519-keypair --file $ED25519_PRIVKEY
 generic-worker new-openpgp-keypair --file $OPENPGP_PRIVKEY
 envsubst < $CONF_PATH/generic-worker.yml.template > $CONF_PATH/generic-worker.yml
 
-# write out key for stackdriver
-echo "$GOOGLE_CREDENTIAL" > /etc/google/auth/application_default_credentials.json
-
 mkdir -p /builds/worker/.android/
 # bitbar mounts this file into root's homedir, but with g-w adb
 # is looking for it worker's homedir
