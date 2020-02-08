9dfc9a09b6ccffc12734b31bacd0f47a7783768b
Andrew Erickson <aerickson@mozilla.com>
2020-02-07 20:41:05 -0800

    get rid of extra newlines

--- taskcluster/script.py
+++ taskcluster/script.py
@@ -68,12 +68,10 @@ def timeout(time):
         # if the timeout is not reached.
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
 
-
 def raise_timeout(signum, frame):
     print("script.py: timeout at %s minutes" % TIMEOUT_MINUTES)
     subprocess.call(["/usr/bin/pstree", "-pct"])
     raise TimeoutError
-
 
 def fatal(message, exception=None, retry=True):
     """Emit an error message and exit the process with status
@@ -90,7 +88,6 @@ def fatal(message, exception=None, retry=True):
         print("{}: {}".format(exception.__class__.__name__, exception))
     sys.exit(exit_code)
 
-
 def show_df():
     try:
         print('\ndf -h\n%s\n\n' % subprocess.check_output(
@@ -98,7 +95,6 @@ def show_df():
             stderr=subprocess.STDOUT).decode())
     except subprocess.CalledProcessError as e:
         print('{} attempting df'.format(e))
-
 
 def get_device_type(device):
     device_type = device.shell_output("getprop ro.product.model", timeout=ADB_COMMAND_TIMEOUT)
@@ -111,7 +107,6 @@ def get_device_type(device):
     else:
         fatal("Unknown device ('%s')! Contact Android Relops immediately." % device_type, retry=False)
     return device_type
-
 
 def enable_charging(device, device_type):
     p2_path = "/sys/class/power_supply/battery/input_suspend"
@@ -152,7 +147,6 @@ def enable_charging(device, device_type):
             "TEST-WARNING | bitbar | Error while attempting to enable charging."
         )
         print("{}: {}".format(e.__class__.__name__, e))
-
 
 def main():
     parser = argparse.ArgumentParser(
