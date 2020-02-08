ddd8930b76f71013e4d0c1494979ccfb88466f89
Andrew Erickson <aerickson@mozilla.com>
2020-02-07 21:45:47 -0800

    tweak pstree outupt

--- taskcluster/script.py
+++ taskcluster/script.py
@@ -50,7 +50,7 @@ class DebugPrinter:
 
     def raise_timeout(self, signum, frame):
         self.print_to_logcat("timeout at %s minute(s)" % TIMEOUT_MINUTES)
-        output = subprocess.getoutput("/usr/bin/pstree -pct")
+        output = subprocess.getoutput("/usr/bin/pstree -aApct")
         self.print_to_logcat(output)
         raise MyTimeoutError
 
