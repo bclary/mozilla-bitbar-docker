99fdba631b3ca1a1de9cec45c2dbe8d36d95002d
Andrew Erickson <aerickson@mozilla.com>
2020-02-11 17:26:42 -0800

    if timed out, exit 1

--- taskcluster/script.py
+++ taskcluster/script.py
@@ -315,6 +315,10 @@ def main():
 
     show_df()
 
+    if rc is None:
+        print("script.py: setting exit code to 1 due to timeout")
+        rc = 1
+
     print('script.py: exiting with exitcode {}.'.format(rc))
     return rc
 
