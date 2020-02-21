96c2a627a7c1e6a4678731b5af0411c2256d6604
Andrew Erickson <aerickson@mozilla.com>
2020-02-21 10:55:52 -0800

    add time

--- tests/sleep_120.sh
+++ tests/sleep_120.sh
@@ -5,4 +5,4 @@ set -x
 
 TIME=120
 
-time /builds/taskcluster/script.py${1} '/bin/bash' '-c' "for i in {1..$TIME}; do echo '444'; sleep 1; done ## run script 10 times"
+/builds/taskcluster/script.py${1} '/bin/bash' '-c' "for i in {1..$TIME}; do echo '444'; sleep 1; done ## run script 10 times"
