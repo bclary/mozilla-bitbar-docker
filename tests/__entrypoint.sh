176c31eb59f418e53fd460184a57458ca0a3c3d0
Andrew Erickson <aerickson@mozilla.com>
2020-02-25 15:07:49 -0800

    tweak

--- tests/__entrypoint.sh
+++ tests/__entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 # original
 #entrypoint.sh
 
-# with timeout
-( entrypoint.sh ) & sleep 10 ; kill $!
+# with timeout (we only need the first parts of entrypoint.sh, not running g-w)
+timeout 6 entrypoint.sh
